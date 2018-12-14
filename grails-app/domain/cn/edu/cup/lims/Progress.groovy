625f41f83ba06be9b32766e2505c403e93bbce09
LiXiaoping <xpli126@126.com>
2018-12-14 18:54:45 +0800

    创建团队成功，但是，指导教师有问题

--- grails-app/domain/cn/edu/cup/lims/Progress.groovy
+++ grails-app/domain/cn/edu/cup/lims/Progress.groovy
@@ -2,13 +2,14 @@ package cn.edu.cup.lims
 
 class Progress {
 
-    Project project
-    Personnel contributor   //贡献者
-    Date registrationTime   //时间
-    String currentStatus    //当前状态
+    Personnel contributor       //贡献者
+    Date registrationTime       //时间
+    String currentStatus        //当前状态
     String problemEncountered   //遇到问题
     String relatedFileName      //相关文件
     Progress upProgress
+
+    static belongsTo = [project: Project]
 
     static constraints = {
         project()
