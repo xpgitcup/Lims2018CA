49d8c71e36dbf669920f61e0a7ad197b3c9588dd
LiXiaoping <xpli126@126.com>
2018-12-22 23:02:07 +0800

    任务分配域类结构调整

--- grails-app/domain/cn/edu/cup/lims/TaskAllocation.groovy
+++ grails-app/domain/cn/edu/cup/lims/TaskAllocation.groovy
@@ -3,14 +3,14 @@ package cn.edu.cup.lims
 class TaskAllocation {
 
     String name
-    PersonTitle personTitle
     ThingType thingType
     String appendFilter
+
+    static hasMany = [personTitles: PersonTitle]
 
     static constraints = {
         name(unique: true)
         thingType()
-        personTitle()
         appendFilter(nullable: true)
     }
 
